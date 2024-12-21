2e1013cc35e9f05ac6ab123d2c4d0e3462d6e3d6
Nikhil John <142223505+nik-johnz@users.noreply.github.com>
2024-12-21 16:38:07 +0530

    Update layout.jsx

--- client/src/components/auth/layout.jsx
+++ client/src/components/auth/layout.jsx
@@ -2,21 +2,14 @@ import { Outlet } from "react-router-dom";
 
 function AuthLayout() {
   return (
-    <div className="min-h-screen w-full bg-cyan">
-      {/* <div className="hidden lg:flex items-center justify-center bg-black w-1/2 px-12">
-        <div className="max-w-md space-y-6 text-center text-primary-foreground">
-          <h1 className="text-4xl font-extrabold tracking-tight">
-            Welcome to Shopiee
-          </h1>
+    <div className="bg-cyan min-h-screen w-full flex justify-center items-center md:bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
+      <div className="grid md:grid-cols-2 justify-center items-center rounded-lg bg-gray-300 shadow-lg bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500">
+        <div className="px-4 py-12 sm:px-6 lg:px-8 ">
+          <h1 className="text-5xl font-extrabold text-center text-white tracking-tight"> Welcome</h1>
         </div>
-      </div> */}
-      <div>
-        <h1 className="text-4xl text-center mt-10 text-black font-extrabold tracking-tight">
-          Welcome to <span className="text-blue">SHOPIEE</span>
-        </h1>
-      </div>
-      <div className="flex flex-1 mt-10 items-center justify-center bg-cyan px-4 py-12 sm:px-6 lg:px-8">
-        <Outlet />
+        <div className="px-4 py-12 sm:px-6 lg:px-8">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
